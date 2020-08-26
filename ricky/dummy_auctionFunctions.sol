2f87af26bfbcac54afe66b8468638f9585788052
rickycg123 <55298354+rickycg123@users.noreply.github.com>
2020-08-25 20:03:44 -0400

    Update dummy_auctionFunctions.sol

--- ricky/dummy_auctionFunctions.sol
+++ ricky/dummy_auctionFunctions.sol
@@ -5,46 +5,46 @@
 
     mapping(uint => FlightAuction) public auctions;
 
-    modifier flightRegistered(uint token_id) {
-        require(_exists(token_id), "Flight not registered!");
+    modifier flightRegistered(uint offer_id) {
+        require(_exists(offer_id), "Flight not registered!");
         _;
     }
 
-    function createAuction(uint token_id) public onlyOwner {
-        auctions[token_id] = new FlightAuction(charterAddress, auctionTimeLength);
+    function createAuction(uint offer_id) public onlyOwner {
+        auctions[offer_id] = new FlightAuction(charterAddress, auctionTimeLength);
     }
 
     function registerFlight(string memory uri) public payable onlyOwner {
-        token_ids.increment();
-        uint token_id = token_ids.current();
-        _mint(charterAddress, token_id);
-        _setTokenURI(token_id, uri);
-        createAuction(token_id);
+        offer_ids.increment();
+        uint offer_id = offer_ids.current();
+        _mint(charterAddress, offer_id);
+        _setTokenURI(offer_id, uri);
+        createAuction(offer_id);
     }
 
 
-    function endAuction(uint token_id) public onlyOwner flightRegistered(token_id) {
-        FlightAuction auction = auctions[token_id];
+    function endAuction(uint offer_id) public onlyOwner flightRegistered(token_id) {
+        FlightAuction auction = auctions[offer_id];
         auction.auctionEnd();
-        safeTransferFrom(owner(), auction.highestBidder(), token_id);
+        safeTransferFrom(owner(), auction.highestBidder(), offer_id);
     }
 
-    function auctionEnded(uint token_id) public view returns(bool) {
-        FlightAuction auction = auctions[token_id];
+    function auctionEnded(uint offer_id) public view returns(bool) {
+        FlightAuction auction = auctions[offer_id];
         return auction.ended();
     }
 
-    function highestBid(uint token_id) public view flightRegistered(token_id) returns(uint) {
-        FlightAuction auction = auctions[token_id];
+    function highestBid(uint offer_id) public view flightRegistered(offer_id) returns(uint) {
+        FlightAuction auction = auctions[offer_id];
         return auction.highestBid();
     }
 
-    function pendingReturn(uint token_id, address sender) public view flightRegistered(token_id) returns(uint) {
-        FlightAuction auction = auctions[token_id];
+    function pendingReturn(uint offer_id, address sender) public view flightRegistered(offer_id) returns(uint) {
+        FlightAuction auction = auctions[offer_id];
         return auction.pendingReturn(sender);
     }
 
-    function bid(uint token_id) public payable flightRegistered(token_id) {
-        FlightAuction auction = auctions[token_id];
+    function bid(uint offer_id) public payable flightRegistered(offer_id) {
+        FlightAuction auction = auctions[offer_id];
         auction.bid.value(msg.value)(msg.sender);
     }
